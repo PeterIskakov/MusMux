f884cce59b389af2e6fa536e49df44141fc87a4e
Peter Iskakov <peter.iskakov@gmail.com>
2025-11-08 21:30:03 -0800

    added some stuff

--- MusMux/MainWindow.xaml.cs
+++ MusMux/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.Core;
+using Windows.Media.Playback;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,9 +25,58 @@ namespace MusMux
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        private List<SongItem> Songs;
+        private int CurrentSong;
+        private MediaPlayer SongPlayer;
         public MainWindow()
         {
+            CurrentSong = 0;
             InitializeComponent();
+            Songs = [
+                new (@"C:\Users\Peter\Music\tron.mp3"),
+                new (@"C:\Users\Peter\Music\clocks.mp3")
+                ];
+            SongPlayer = new();
+            SongPlayer.MediaEnded += SongPlayer_MediaEnded;
+            SongPlayer.Source = MediaSource.CreateFromUri(new Uri(Songs[CurrentSong].Path));
+            BaseExample.ItemsSource = Songs;
+        }
+
+        private void SongPlayer_MediaEnded(MediaPlayer sender, object args)
+        {
+            MainPlay.Icon = new SymbolIcon(Symbol.Play);
+            MainPlay.Label = "Play";
+            SongPlayer.Pause();
+            SongPlayer.AutoPlay = false;
+        }
+
+        private void MainPlay_Click(object sender, RoutedEventArgs e)
+        {
+            AppBarButton s = (AppBarButton)sender;
+            if (SongPlayer.CurrentState == MediaPlayerState.Playing)
+            {
+                s.Icon = new SymbolIcon(Symbol.Play);
+                s.Label = "Play";
+                SongPlayer.Pause();
+                SongPlayer.AutoPlay = false;
+                return;
+            }
+            s.Icon = new SymbolIcon(Symbol.Pause);
+            s.Label = "Pause";
+            SongPlayer.Play();
+            SongPlayer.AutoPlay = true;
+        }
+
+        private void MainPrev_Click(object sender, RoutedEventArgs e)
+        {
+            if (CurrentSong - 1 >= 0) CurrentSong--;
+            SongPlayer.Source = MediaSource.CreateFromUri(new Uri(Songs[CurrentSong].Path));
+        }
+
+        private void MainNext_Click(object sender, RoutedEventArgs e)
+        {
+            if (CurrentSong + 1 < Songs.Count) CurrentSong++;
+            SongPlayer.Source = MediaSource.CreateFromUri(new Uri(Songs[CurrentSong].Path));
         }
     }
 }
---
+++ MusMux/SongItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.FileProperties;
+
+namespace MusMux
+{
+    class SongItem
+    {
+        public TagLib.Tag Properties { get; private set; }
+        public string Path { get; private set; }
+
+        public SongItem(string path)
+        {
+            Path = path;
+            TagLib.File file = TagLib.File.Create(path);
+            Properties = file.Tag;
+        }
+    }
+}
